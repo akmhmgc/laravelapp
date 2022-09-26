a096373a5e6944b72ba1a217cd92d53ab65e963d
akmhmgc <akimuhamaguchi@icloud.com>
2022-09-26 22:23:39 +0900

    パラメータ付きのページを作成

--- routes/web.php
+++ routes/web.php
@@ -17,11 +17,26 @@ Route::get('/', function () {
     return view('welcome');
 });
 // 無名関数の返り値にhtmlを返せばOK
-Route::get('/hello', function () {
-    return '<html>
-        <body>
-            <h1>Hello</h1>
-            <p>This is sample page</p>
-        </body>
-    </html>';
+Route::get('hello/{msg}', function ($msg) {
+
+    $html = <<<EOF
+    <html>
+    <head>
+    <title>Hello</title>
+    <style>
+    body {font-size:16pt; color:#999; }
+    h1 { font-size:100pt; text-align:right; color:#eee;
+       margin:-40px 0px -50px 0px; }
+    </style>
+    </head>
+    <body>
+       <h1>Hello</h1>
+       <p>{$msg}</p>
+       <p>これは、サンプルで作ったページです。</p>
+    </body>
+    </html>
+    EOF;
+
+    // htmlコードを返す
+    return $html;
 });
