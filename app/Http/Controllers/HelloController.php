b46cf4620344413e4a182dab30f550b7a7670300
akmhmgc <akimuhamaguchi@icloud.com>
2022-09-28 09:16:31 +0900

    ルートパラメータを受け取る

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -8,10 +8,11 @@ use Illuminate\Http\Response;
 class HelloController extends Controller
 {
 
-    public function index()
+    public function index($id = 'zero')
     {
         $data = [
-            'msg' => 'これはコントローラーから渡されたメッセージです'
+            'msg' => 'これはコントローラーから渡されたメッセージです',
+            'id' =>  $id,
         ];
         return view('hello.index', $data);
     }
