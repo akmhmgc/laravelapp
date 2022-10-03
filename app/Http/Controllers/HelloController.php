ac8793b263f00dd34196c5f36a17b4ab7d3405ab
akmhmgc <akimuhamaguchi@icloud.com>
2022-10-03 09:36:05 +0900

    show data from db

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -6,22 +6,15 @@ use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use App\Http\Requests\HelloRequest;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\DB;
 
 class HelloController extends Controller
 {
 
     public function index(Request $request)
     {
-        $validator = Validator::make($request->query(), [
-            'id' => 'required',
-            'pass' => 'required',
-        ]);
-        if ($validator->fails()) {
-            $msg = 'クエリーに問題があります。';
-        } else {
-            $msg = 'ID/PASSを受け付けました。フォームを入力下さい。';
-        }
-        return view('hello.index', ['msg' => $msg,]);
+        $items = DB::select('select * from people');
+        return view('hello.index', ['items' => $items]);
     }
 
     public function post(Request $request)
