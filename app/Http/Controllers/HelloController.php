c39d2acbfe4e64e4efd7868eecd1a1e18f3fc958
akmhmgc <akimuhamaguchi@icloud.com>
2022-10-05 21:00:48 +0900

    Add edit and update

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -13,29 +13,47 @@ class HelloController extends Controller
 
     public function index(Request $request)
     {
-        if(isset($request->id)){
-            $params = ['id'=> $request->id];
-            $items = DB::select('select * from people where id = :id', $params);
-        }else{
-            $items = DB::select('select * from people');
-        }
+        $items = DB::select('select * from people');
         return view('hello.index', ['items' => $items]);
     }
 
     public function post(Request $request)
     {
-        $validator = Validator::make($request->all(), [
-            'name' => 'required',
-            'mail' => 'email',
-            'age' => 'numeric|between:0,150',
-        ]);
-        // バリデーションに失敗した時の処理
-        if ($validator->fails()) {
-            return redirect('/hello') // リダイレクトする場所を表示
-                ->withErrors($validator)
-                ->withInput(); // 入力されたinputをそのまま付加してリダイレクトする
-        }
+        $items = DB::select('select * from people');
+        return view('hello.index', ['items' => $items]);
+    }
 
-        return view('hello.index', ['msg' => '正しく入力されました！']);
+    public function add(Request $request)
+    {
+        return view('hello.add');
+    }
+
+    public function create(Request $request)
+    {
+        $param = [
+            'name' => $request->name,
+            'mail' => $request->mail,
+            'age' => $request->age,
+        ];
+        DB::insert('insert into people (name, mail, age) values (:name, :mail, :age)', $param);
+        return redirect('/hello');
+    }
+    public function edit(Request $request)
+    {
+        $param = ['id' => $request->id];
+        $item = DB::select('select * from people where id = :id', $param);
+        return view('hello.edit', ['form' => $item[0]]);
+    }
+
+    public function update(Request $request)
+    {
+        $param = [
+            'id' => $request->id,
+            'name' => $request->name,
+            'mail' => $request->mail,
+            'age' => $request->age,
+        ];
+        DB::update('update people set name =:name, mail = :mail, age = :age where id = :id', $param);
+        return redirect('/hello');
     }
 }
