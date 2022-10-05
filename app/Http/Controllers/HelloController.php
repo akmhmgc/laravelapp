846e0f443e1f0665b2dee7aeca699623fb41ddc2
akmhmgc <akimuhamaguchi@icloud.com>
2022-10-05 21:13:34 +0900

    クエリビルダーへの置き換え

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -13,7 +13,9 @@ class HelloController extends Controller
 
     public function index(Request $request)
     {
-        $items = DB::select('select * from people');
+        // $items = DB::select('select * from people');
+        // クエリビルダへの置き換え
+        $items = DB::table('people')->get();
         return view('hello.index', ['items' => $items]);
     }
 
