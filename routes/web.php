3898303117edb44da14a308578061b5522f3dbd5
akmhmgc <akimuhamaguchi@icloud.com>
2022-10-06 09:37:42 +0900

    Add search page

--- routes/web.php
+++ routes/web.php
@@ -31,3 +31,5 @@ Route::get('hello/del', [HelloController::class, 'del']);
 Route::post('hello/del', [HelloController::class, 'remove']);
 
 Route::get('person', [PersonController::class, 'index']);
+Route::get('person/find', [PersonController::class, 'find']);
+Route::post('person/find', [PersonController::class, 'search']);
