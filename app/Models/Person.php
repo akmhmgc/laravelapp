2a309a4602e871fca5586039a3d38c0af90ed96d
akmhmgc <akimuhamaguchi@icloud.com>
2022-10-06 09:58:03 +0900

    Add scope

--- app/Models/Person.php
+++ app/Models/Person.php
@@ -11,4 +11,21 @@ class Person extends Model
     {
         return $this->id . ': ' . $this->name . ' (' . $this->age . ')';
     }
+
+    // スコープの追加
+    public function scopeNameEqual($query, $str)
+    {
+        eval(\Psy\sh());
+        return $query->where('name', $str);
+    }
+
+    public function scopeAgeGreaterThan($query, $n)
+    {
+        return $query->where('age', '>=', $n);
+    }
+
+    public function scopeAgeLessThan($query, $n)
+    {
+        return $query->where('age', '<=', $n);
+    }
 }
