cc38d73f243dcab596e880da2e2ee8d895aed4a5
akmhmgc <akimuhamaguchi@icloud.com>
2022-10-08 17:02:42 +0900

    Add board add view

--- resources/views/board/index.blade.php
+++ resources/views/board/index.blade.php
@@ -3,21 +3,19 @@
 @section('title', 'Board.index')
 
 @section('menubar')
-@parent
-ボード・ページ
+   @parent
+   ボード・ページ
 @endsection
 
 @section('content')
-<table>
-    <tr>
-        <th>Data</th>
-    </tr>
-    @foreach ($items as $item)
-    <tr>
-        <td>{{$item->getData()}}</td>
-    </tr>
-    @endforeach
-</table>
+   <table>
+   <tr><th>Data</th></tr>
+   @foreach ($items as $item)
+       <tr>
+           <td>{{$item->getData()}}</td>
+       </tr>
+   @endforeach
+   </table>
 @endsection
 
 @section('footer')
